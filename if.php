36cd31fca0cc8472892828b9460a7369713f27f9
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-03 15:35:21 -0600

    Shortened file using if/elseif/else statements.

--- if.php
+++ if.php
@@ -1,47 +1,29 @@
- <?php
+<?php
 
- $a = 5;
- $b = 10;
- $c = '10';
+$a = 5;
+$b = 10;
+$c = '10';
 
- if ($a < $b) {
-     // output the appropriate result
-     echo "$a is < $b\n";
- }
+if ($a < $b) {
+	echo "$a is < $b\n";
+}else {
+	echo "$a is > $b\n";
+}
 
- if ($b > $a) {
-     // output the appropriate result
-     echo "$b is > $a\n";
- }
+if ($b >= $c) {
+	echo "$b is >= $c\n";
+}else {
+	echo "$b is <= $c\n";
+}
 
- if ($b >= $c) {
-     // output the appropriate result
-     echo "$b is >= $c\n";
- }
+if ($b == $c) {
+	echo "$b is == $c\n";
+}else if ($b === $c) {
+	echo "$b is === $c\n";
+}else if ($b != $c) {
+	echo "$b is not equal to $c\n";
+}else ($b !== $c) {
+	echo "$b is not identical to $c\n";
+}
 
- if ($b <= $c) {
-     // output the appropriate result
-     echo "$b is <= $c\n";
- }
-
- if ($b == $c) {
-     // output the appropriate result
-     echo "$b is == $c\n";
- }
-
- if ($b === $c) {
-     // output the appropriate result
-     echo "$b is === $c\n";
- }
-
- // output the appropriate result
- if ($b != $c) {
-     echo "$b is not equal to $c\n";
- }
-
- // output the appropriate result
- if ($b !== $c) {
-     echo "$b is not identical to $c\n";
- }
-
- ?>
+?>
