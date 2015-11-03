868607b552c1ddacf3b433aa1e727d58e9aedb27
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-03 16:44:25 -0600

    Modified fizzbuzz.php to echo "FizzBuzz" for 3's and 5's.

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -2,12 +2,12 @@
 $test = 0;
 while ($test < 100){
 	$test++;
-	if ($test % 3 == 0){
+	if ($test % 3 == 0 && $test % 5 == 0){
+		echo "FizzBuzz" . PHP_EOL;
+	} else if ($test % 3 == 0){
 		echo "Fizz" . PHP_EOL;
 	} else if ($test % 5 == 0){
 		echo "Buzz" . PHP_EOL;
-	} else if ($test % 3 == 0 && $test % 5 == 0){
-		echo "FizzBuzz" . PHP_EOL;
 	} else {
 		echo "$test" . PHP_EOL;
 	}
