f4de259fadbb43bcdd25bd459bf1d879f022b6b4
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-03 12:06:46 -0600

    PHP exercise using if statements and operators.

---
+++ if.php
@@ -0,0 +1,47 @@
+ <?php
+
+ $a = 5;
+ $b = 10;
+ $c = '10';
+
+ if ($a < $b) {
+     // output the appropriate result
+     echo "$a is < $b\n";
+ }
+
+ if ($b > $a) {
+     // output the appropriate result
+     echo "$b is > $a\n";
+ }
+
+ if ($b >= $c) {
+     // output the appropriate result
+     echo "$b is >= $c\n";
+ }
+
+ if ($b <= $c) {
+     // output the appropriate result
+     echo "$b is <= $c\n";
+ }
+
+ if ($b == $c) {
+     // output the appropriate result
+     echo "$b is == $c\n";
+ }
+
+ if ($b === $c) {
+     // output the appropriate result
+     echo "$b is === $c\n";
+ }
+
+ // output the appropriate result
+ if ($b != $c) {
+     echo "$b is not equal to $c\n";
+ }
+
+ // output the appropriate result
+ if ($b !== $c) {
+     echo "$b is not identical to $c\n";
+ }
+
+ ?>
