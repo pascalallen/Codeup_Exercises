0c27a8a2cbeff2c38ccb95d7921373bf85d2942a
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-04 15:23:58 -0600

    Completed for.php fizzbuzz exercise. Used for loop/if/else etc.

--- for.php
+++ for.php
@@ -1,1 +1,19 @@
 <?php
+if($argc != 3){
+	die("Wrong number of arguments!" . PHP_EOL);	
+}
+$start = $argv[1];
+$end = $argv[2];
+for ($start; $start <= $end; $start++){
+	if ($start % 3 == 0 && $start % 5 == 0){
+		fwrite(STDOUT, "FizzBuzz" . PHP_EOL);
+	}
+	else if ($start % 3 == 0){
+		fwrite(STDOUT, "Fizz" . PHP_EOL);
+	}
+	else if ($start % 5 == 0){
+		fwrite(STDOUT, "Buzz" . PHP_EOL);
+	} else {
+		fwrite(STDOUT, "$start" . PHP_EOL);
+	}
+}
