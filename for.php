2fe3e36e39cd9829aa82b3d438d4af9c968cd940
Thomas Allen <thomasallen@Thomass-MBP.attlocal.net>
2015-11-04 17:22:12 -0600

    added if statement to default to 1 and 100 for [1] and argv[2] if user does not specify min and max values

--- for.php
+++ for.php
@@ -1,17 +1,16 @@
 <?php
-if($argc != 3){
-	die("Wrong number of arguments!" . PHP_EOL);	
+if(!$argv[1] && !$argv[2]){
+	$argv[1] = 1;
+	$argv[2] = 100;
 }
 $start = $argv[1];
 $end = $argv[2];
 for ($start; $start <= $end; $start++){
 	if ($start % 3 == 0 && $start % 5 == 0){
 		fwrite(STDOUT, "FizzBuzz" . PHP_EOL);
-	}
-	else if ($start % 3 == 0){
+	} else if ($start % 3 == 0){
 		fwrite(STDOUT, "Fizz" . PHP_EOL);
-	}
-	else if ($start % 5 == 0){
+	} else if ($start % 5 == 0){
 		fwrite(STDOUT, "Buzz" . PHP_EOL);
 	} else {
 		fwrite(STDOUT, "$start" . PHP_EOL);
