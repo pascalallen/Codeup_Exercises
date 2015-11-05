7f42450a650a7705726168fbe41f886688337f70
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-05 15:54:04 -0600

    Incomplete exercise. Debugging. Need to get only Thursday to print out in if statement

---
+++ switch.php
@@ -0,0 +1,57 @@
+<?php
+date_default_timezone_set('America/Chicago');
+
+// Get Day of Week as number
+// 1 (for Monday) through 7 (for Sunday)
+$dayOfWeek = date('N');
+
+switch($dayOfWeek) {
+	case 1:
+		echo "Monday\n";
+		break;
+	case 2:
+		echo "Tuesday\n";
+		break;
+	case 3:
+		echo "Wednesday\n";
+		break;
+	case 4:
+		echo "Thursday\n";
+		break;
+	case 5:
+		echo "Friday\n";
+		break;
+	case 6:
+		echo "Saturday\n";
+		break;
+	case 7:
+		echo "Sunday\n";
+		break;
+	default:
+		break;
+}
+
+if($daysOfWeek == 1){
+	echo "Monday\n";
+}else if($daysOfWeek == 2){
+	echo "Tuesday\n";
+}else if($daysOfWeek == 3){
+	echo "Wednesday\n";
+}else if($daysOfWeek == 4){
+	echo "Thursday\n";
+}else if($daysOfWeek == 5){
+	echo "Friday\n";
+}else if($daysOfWeek == 6){
+	echo "Saturday\n";
+}else if($daysOfWeek == 7){
+	echo "Sunday\n";
+}
+
+
+
+
+
+
+
+
+
