9d1e72aed7f4d2d2d2f1586e376c94803511fbaa
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-06 09:21:53 -0600

    Refactored code to check for numeric values passed into functions

--- arithmetic.php
+++ arithmetic.php
@@ -2,27 +2,47 @@
 
 function add($a, $b)
 {
-    return $a + $b;
+	if (is_numeric($a) && is_numeric($b)) {
+	    return $a + $b;
+	} else {
+	    return "ERROR: Both arguments must be numbers\n";
+	}
 }
 
 function subtract($a, $b)
 {
-	return $a - $b;
+	if (is_numeric($a) && is_numeric($b)) {
+        return $a - $b;
+    } else {
+        return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 function multiply($a, $b)
 {
-    return $a * $b;
+    if (is_numeric($a) && is_numeric($b)) {
+        return $a * $b;
+    } else {
+        return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 function divide($a, $b)
 {
-    return $a / $b;
+    if (is_numeric($a) && is_numeric($b) && $a && $b != 0) {
+        return $a / $b;
+    } else {
+        return "ERROR: Both arguments must be numbers AND not 0\n";
+    }
 }
 
 function modulus($a, $b)
 {
-	return $a % $b;
+	if (is_numeric($a) && is_numeric($b)) {
+        return $a % $b;
+    } else {
+        return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 // Add code to test your functions here
@@ -31,3 +51,8 @@ echo subtract(10, 2) . PHP_EOL;
 echo multiply(10, 2) . PHP_EOL;
 echo divide(10, 2) . PHP_EOL;
 echo modulus(10, 2) . PHP_EOL;
+
+
+
+
+
