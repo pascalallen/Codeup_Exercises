193c3ae40df42c705d66a8ef937055016cd8f105
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-06 08:42:35 -0600

    Completed exercise

--- arithmetic.php
+++ arithmetic.php
@@ -20,5 +20,14 @@ function divide($a, $b)
     return $a / $b;
 }
 
+function modulus($a, $b)
+{
+	return $a % $b;
+}
+
 // Add code to test your functions here
-echo divide(10, 2);
+echo add(10, 2) . PHP_EOL;
+echo subtract(10, 2) . PHP_EOL;
+echo multiply(10, 2) . PHP_EOL;
+echo divide(10, 2) . PHP_EOL;
+echo modulus(10, 2) . PHP_EOL;
