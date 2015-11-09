238b669b82e505a6f5129bbdee7cf03123115f16
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-09 14:48:17 -0600

    Almost finished. Working on adding and inside echo string.

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,1 +1,20 @@
-<?php
+ <?php
+
+ // Converts array into list n1, n2, ..., and n3
+ function humanizedList($array) {
+   // Your solution goes here!
+ 	$array = implode(', ', $array);
+ 	return $array;
+ }
+
+ // List of famous peeps
+ $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+ // TODO: Convert the string into an array
+ $physicistsArray = explode(', ', $physicistsString);
+
+ // Humanize that list
+ $famousFakePhysicists = humanizedList($physicistsArray);
+
+ // Output sentence
+ echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
