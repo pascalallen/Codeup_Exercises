0c55ba6947122c9c514ce8ce8311c13caddbdd87
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-10 14:15:10 -0600

    Notated exercise. Happy with notes on file.

--- contact-parser.php
+++ contact-parser.php
@@ -4,16 +4,18 @@ function parseContacts($filename) // function to parse contacts.txt file
     $handle = fopen($filename, "r"); // opens contacts.txt file and is a read only
     $fileContentsString = fread($handle, filesize($filename)); // reading to the specific length of the contacts.txt file
     $arrayOfStrings = explode("\n", trim($fileContentsString)); // takes apart each line in contacts.txt file
-    foreach($arrayOfStrings as $info) // going through contacts array
+    foreach($arrayOfStrings as $info) // going through contacts.txt file which is an array
     {
-    	$name = explode('|', $info); // taking apart each line at the '|' to end up with the name as index name[0] and the number as index name[1]
-    	$number = substr($name[1], 0, 3) . '-' . substr($name[1], 3, 3) . '-' . substr($name[1], 6, 4); // creating the phone numbers on each line with dashes
-    	$contacts[] = array( // renaming name[0] to 'name' and name[1] to number and pushing it to contacts[]
-    		'name' => $name[0], 
-    		'number' => $number
+    	$personInfoArray = explode('|', $info); // taking apart each line at the '|'. left with 2 parts, name and phone number
+    	$name = $personInfoArray[0]; // assigning $name to the names before the '|'
+    	$number = $personInfoArray[1]; // assigning $number to the numbers after the '|'
+    	$number = substr($personInfoArray[1], 0, 3) . '-' . substr($personInfoArray[1], 3, 3) . '-' . substr($personInfoArray[1], 6, 4); // creating the phone numbers on each line with dashes
+    	$contacts[] = array( // creating $contacts array
+    		'name' => $name, // declaring index[0] of the $contacts array to be assigned 'name'
+    		'number' => $number // declaring index[1] of the $contacts array to be assigned 'number'
     		);
     };
     fclose($handle); // closing file that was opened and read('contacts.txt')
     return $contacts; // returns newly built contacts array after the foreach loop has created it
 }
-var_dump(parseContacts('contacts.txt')); // dumping the contacts.txt as arrays
+var_dump(parseContacts('contacts.txt')); // dumping the contacts.txt as array
