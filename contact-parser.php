5eec5f17dd809603f8e4c4ee322ab6f0349226cc
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-10 11:37:32 -0600

    Completed exercise.

--- contact-parser.php
+++ contact-parser.php
@@ -1,18 +1,18 @@
 <?php
-$filename = 'contacts.txt';
-function parseContacts($filename)
+function parseContacts($filename) // function to parse contacts.txt file
 {
-	$handle = fopen($filename, 'r');
-	$contents = fread($handle, filesize($filename));
-	$contactsArray = explode(PHP_EOL, $contents);
-    $contacts = array();
-    foreach($contactsArray as $key => $contact) {
-    	$innerArray['name'] = explode('|', $contact)[0];
-    	$innerArray['number'] = explode('|', $contact)[1];
-    	array_push($contacts, $innerArray);
+    $handle = fopen($filename, "r"); // opens contacts.txt file and is a read only
+    $fileContentsString = fread($handle, filesize($filename)); // reading to the specific length of the contacts.txt file
+    $arrayOfStrings = explode("\n", trim($fileContentsString)); // takes apart each line in contacts.txt file
+    foreach($arrayOfStrings as $info) {
+    	$name = explode('|', $info);
+    	$number = substr($name[1], 0, 3) . '-' . substr($name[1], 3, 3) . '-' . substr($name[1], 6, 4);
+    	$contacts[] = array(
+    		'name' => $name[0], 
+    		'number' => $number
+    		);
     };
     fclose($handle);
     return $contacts;
 }
-
 var_dump(parseContacts('contacts.txt'));
