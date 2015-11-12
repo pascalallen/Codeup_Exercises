99b0237568e11f1dcf7ea43e7a89d2fcb0dced5c
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-12 11:19:01 -0600

    Made some minor changes.

--- logger.php
+++ logger.php
@@ -4,12 +4,11 @@ function logMessage($logLevel, $message)
 {
 	$logDate = date("Y-m-d");
     $filename = "log-{$logDate}.log";
-    $fullMessage = date("Y-m-d H:i:s ") . $logLevel . $message;
+    $fullMessage = date("Y-m-d h:i:s ") . $logLevel . $message;
 	$handle = fopen($filename, 'a');
 	fwrite($handle, $fullMessage . PHP_EOL);
 	fclose($handle);
 }
-
 
 function logInfo($message) 
 {
