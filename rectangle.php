96f0972e0584a27cfdfd7045c94f5a5df9e20a61
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-12-04 14:42:04 -0600

    Started inheritance exercise.

---
+++ rectangle.php
@@ -0,0 +1,32 @@
+class Rectangle
+{
+    public $height;
+    public $width;
+
+    public function __construct($height, $width)
+    {
+        $this->height = $height;
+        $this->width  = $width;
+    }
+
+    public function area()
+    {
+        return $this->height * $this->width;
+    }
+}
+
+class Superhero extends Person
+{
+    public $pseudonym;
+    public $capeColor;
+
+    public function alterEgo()
+    {
+        return 'Top Secret Alternate Ego: ' . $this->fullName();
+    }
+
+    public function hasCape()
+    {
+        return !empty($this->capeColor);
+    }
+}
